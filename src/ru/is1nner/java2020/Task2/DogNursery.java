998c6c79eda5b1dc42e3013adcef8c704ee3605f
IS1nner <ilyas1nners@gmail.com>
2020-10-03 10:53:04 +0300

    Добавил питомник. Добавил возраст для 3-й собаки.

---
+++ src/ru/is1nner/java2020/Task2/DogNursery.java
@@ -0,0 +1,17 @@
+package ru.is1nner.java2020.Task2;
+
+public class DogNursery {
+    int count = 10;
+    int last = 0;
+    Dog[] dogs = new Dog[count];
+
+    public void addDog(Dog dog) {
+        dogs[last] = dog;
+        last++;
+    }
+
+    public Dog[] getDogsList() {
+        System.out.println("Nursery");
+        return dogs;
+    }
+}
--- src/ru/is1nner/java2020/Task2/DogTest.java
+++ src/ru/is1nner/java2020/Task2/DogTest.java
@@ -3,7 +3,7 @@ public class DogTest {
     public static void main(String[] args) {
         Dog D1 = new Dog("Bobik",1);
         Dog D2 = new Dog("Lelik",5);
-        Dog D3 = new Dog("Snejok");
+        Dog D3 = new Dog("Snejok",2);
         System.out.println(D1);
         System.out.println(D2);
         System.out.println(D3);
